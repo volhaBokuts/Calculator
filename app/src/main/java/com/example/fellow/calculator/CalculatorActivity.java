98d16becc6c92587e7a715fec7b20f44ef63b6ff
Olga <bokut.olya@gmail.com>
2016-06-08 23:03:43 +0300

    Declare variables (textView, buttins), methods onClick

--- app/src/main/java/com/example/fellow/calculator/CalculatorActivity.java
+++ app/src/main/java/com/example/fellow/calculator/CalculatorActivity.java
@@ -1,13 +1,49 @@
 package com.example.fellow.calculator;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class CalculatorActivity extends AppCompatActivity {
+public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView calcField, inputField;
+    Button clear, negative, percent, divide, multiply, minus, plus, equal, point;
+    Button one, two, three, four, five, six, seven, eight, nine, zero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        inputField = (TextView) findViewById(R.id.result);
+        clear = (Button) findViewById(R.id.clear);
+        divide = (Button) findViewById(R.id.divide);
+        multiply = (Button) findViewById(R.id.multiply);
+        minus = (Button) findViewById(R.id.minus);
+        plus = (Button) findViewById(R.id.plus);
+        equal = (Button) findViewById(R.id.equal);
+        one = (Button) findViewById(R.id.one);
+        two = (Button) findViewById(R.id.two);
+        three = (Button) findViewById(R.id.three);
+        four = (Button) findViewById(R.id.four);
+        five = (Button) findViewById(R.id.five);
+        six = (Button) findViewById(R.id.six);
+        seven = (Button) findViewById(R.id.seven);
+        eight = (Button) findViewById(R.id.eight);
+        nine = (Button) findViewById(R.id.nine);
+        zero = (Button) findViewById(R.id.zero);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    public void onClickClear(View button) {
+        inputField.setText("1");
     }
 }
